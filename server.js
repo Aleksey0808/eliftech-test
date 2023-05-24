542062541575b36de20f3d3ac12a4d37b34f4fe5
Aleksey <103374981+Aleksey0808@users.noreply.github.com>
2023-05-24 20:54:05 +0300

    add db-host

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const app = require('./app')
 const { DB_HOST, PORT = 3000 } = process.env
-// WI09JrGVP0yzSGSO
+//Z4ZhsD48jQF6Bhzq
 
 mongoose
   .connect(DB_HOST)
